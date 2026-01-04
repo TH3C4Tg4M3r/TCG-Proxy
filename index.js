0e9c5cd486a79fc5680782acb25b0347dbf009e6
TH3C@g4M3r <194850223+TH3C4Tg4M3r@users.noreply.github.com>
2026-01-03 20:13:00 -0500

    fix cheerio thing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import { URL } from "url";
 
 const app = express();
